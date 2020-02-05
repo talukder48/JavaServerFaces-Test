91882f80fdea076a649ec9df4160d8d7912db807
Mosharraf <talukder.csedu@gmail.com>
2020-02-05 17:57:28 +0600

    Service

---
+++ src/validator/GLSetupValidation.java
@@ -0,0 +1,12 @@
+package validator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GLSetupValidation {
+	Map<String, String> ResultMap = new HashMap<String, String>();
+
+	public GLSetupValidation() {
+		ResultMap.clear();
+	}
+}
--- src/validator/LoginValidation.java
+++ src/validator/LoginValidation.java
@@ -4,7 +4,6 @@ package validator;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
